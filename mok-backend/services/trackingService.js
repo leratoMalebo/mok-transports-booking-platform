ed7751244885501332f27d379f80324e029f5837
Lerato Malebo <thakanentlele209@gmail.com>
2026-05-26 11:44:09 +0200

    Using the actual waybill no from JKJ to test tracking

--- mok-backend/services/trackingService.js
+++ mok-backend/services/trackingService.js
@@ -66,7 +66,7 @@ async function trackShipment(trackingNo) {
                 'Waybill',
                 'getEvents',
                 {
-                    trackno: actualTrackNo
+                    waybillno: 'MTSMOK000009'
                 }
             );
 
